b4fa747425327f475a63b66e6cb5fa489f07467a
Dinson Davis <117897698+Dins2344@users.noreply.github.com>
2023-10-06 22:01:43 +0530

    about page minor change

--- pages/about/index.js
+++ pages/about/index.js
@@ -139,7 +139,7 @@ const About = () => {
           >
             when an unknown printer took a galley of type and scrambled it to
             make a type specimen book. It has survived not only five centuries,
-            but also the leap into
+            
           </motion.p>
 
           {/* counters */}
