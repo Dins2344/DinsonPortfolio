2e9ac8d95403708178208163079dbd81b0ade340
Dinson Davis <117897698+Dins2344@users.noreply.github.com>
2023-10-17 23:13:49 +0530

    service page done

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -1,10 +1,10 @@
 //import swiper react components
-import { Swiper, SwiperSlide } from 'swiper/react'
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // import swiper styles
-import 'swiper/css'
-import 'swiper/css/free-mode'
-import 'swiper/css/pagination'
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
 
 // icons
 import {
@@ -13,40 +13,40 @@ import {
   RxDesktop,
   RxReader,
   RxRocket,
+  RxArrowTopRight,
 } from "react-icons/rx";
-
 
 // data
 const serviceData = [
   {
     icon: <RxCrop />,
-    title: 'Branding',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "Branding",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
   {
     icon: <RxPencil2 />,
-    title: 'Design',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "Design",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
   {
     icon: <RxDesktop />,
-    title: 'Development',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "Development",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
   {
     icon: <RxReader />,
-    title: 'Copywriting',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "Copywriting",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
   {
     icon: <RxRocket />,
-    title: 'SEO',
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    title: "SEO",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
   },
 ];
 
 //import swiper modules
-import {FreeMode, Pagination} from 'swiper'
+import { FreeMode, Pagination } from "swiper";
 
 const ServiceSlider = () => {
   return (
@@ -54,33 +54,43 @@ const ServiceSlider = () => {
       breakpoints={{
         320: {
           slidesPerView: 1,
-          spaceBetween:15
+          spaceBetween: 15,
         },
         640: {
           slidesPerView: 3,
-          spaceBetween:15,
-        }
+          spaceBetween: 15,
+        },
       }}
       freeMode={true}
       pagination={{
-        clickable:true
+        clickable: true,
       }}
-      modules={{ FreeMode, Pagination }}
-      className='h-[240px] sm:h-[340px]'
+      modules={[ FreeMode, Pagination ]}
+      className="h-[240px] sm:h-[340px]"
     >
-      {
-        serviceData.map((item, i) => {
-          return (
-            <SwiperSlide key={i}>
-              <div>
-                
+      {serviceData.map((item, i) => {
+        return (
+          <SwiperSlide key={i}>
+            <div className="bg-[rgba(65,47,123,0.15)] h-max rounded-lg px-6 py-8 flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer hover:bg[rgba(89,65,169,0.15)] transition-all duration-300">
+              {/* icon  */}
+              <div className="text-4xl text-accent mb-4">{item.icon}</div>
+              {/*title & descs */}
+              <div className="mb-8">
+                <div className="mb-2 text-lg">{item.title}</div>
+                <p className="max-w-[350px] leading-normal">
+                  {item.description}
+                </p>
               </div>
-            </SwiperSlide>
-          )
-        })
-      }
+              {/* arrow */}
+              <div className="text-3xl">
+                <RxArrowTopRight className="group-hover:rotate-45 group-hover:text-accent transition-all duration-300" />
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
     </Swiper>
-  )
+  );
 };
 
 export default ServiceSlider;
--- pages/services/index.js
+++ pages/services/index.js
@@ -18,20 +18,40 @@ const Services = () => {
     <div className="h-full bg-primary/30 py-36 flex items-center">
       <Circles />
       <div className="container mx-auto">
-        <div className="flex flex-col xl:flex-row gap-x-8"> 
+        <div className="flex flex-col xl:flex-row gap-x-8">
           {/* text */}
           <div className="text-center flex xl:w-[30vw] flex-col lg:text-left mb-4 xl:mb-0">
-            <h2 className="h2 xl:mt-8">
+            <motion.h2
+              variants={fadeIn("up", 0.2)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="h2 xl:mt-8"
+            >
               My services <span className="text-accent">.</span>
-            </h2>
-            <p className="mb-4 max-w-[400px] mx-auto lg:mx-0">
+            </motion.h2>
+            <motion.p
+              variants={fadeIn("up", 0.4)}
+              initial="hidden"
+              animate="show"
+              exit="hidden"
+              className="mb-4 max-w-[400px] mx-auto lg:mx-0"
+            >
               but also the leap into electronic typesetting, remaining
               essentially unchanged. It was popularised in the 1960s with the
               release of Letraset sheets
-            </p>
+            </motion.p>
           </div>
-          {/* slider */}
-          <ServiceSlider />
+          <motion.div
+            variants={fadeIn("up", 0.6)}
+            initial="hidden"
+            animate="show"
+            exit="hidden"
+            className="w-full xl:max-w-[65%]"
+          >
+            {/* slider */}
+            <ServiceSlider />
+          </motion.div>
         </div>
       </div>
       <Bulb />
