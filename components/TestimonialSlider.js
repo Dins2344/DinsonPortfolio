f0d66d8f561dca03081bb8f14e100f8297e1765b
Dinson Davis <117897698+Dins2344@users.noreply.github.com>
2023-11-01 23:30:30 +0530

    testimonial v1

--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -1,30 +1,86 @@
 // testimonial data
 const testimonialData = [
   {
-    image: '/t-avt-1.png',
-    name: 'Anne Smith',
-    position: 'Customer',
+    image: "/t-avt-1.png",
+    name: "Anne Smith",
+    position: "Customer",
     message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!",
   },
   {
-    image: '/t-avt-2.png',
-    name: 'Jane Doe',
-    position: 'Customer',
+    image: "/t-avt-2.png",
+    name: "Jane Doe",
+    position: "Customer",
     message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!",
   },
   {
-    image: '/t-avt-3.png',
-    name: 'Jhon Doe',
-    position: 'Customer',
+    image: "/t-avt-3.png",
+    name: "Jhon Doe",
+    position: "Customer",
     message:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!',
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!",
   },
 ];
 
+// next Image
+import Image from "next/image";
+//import swiper react components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// import swiper styles
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+
+// icons
+import { FaQuoteLeft } from "react-icons/fa";
+//import swiper modules
+import { Pagination } from "swiper";
+
 const TestimonialSlider = () => {
-  return <div>Testimonial Slider</div>;
+  return (
+    <Swiper
+      pagination={{
+        clickable: true,
+      }}
+      modules={[Pagination]}
+      className="h-[280px] sm:h-[480px]"
+    >
+      {testimonialData.map((person, i) => {
+        return (
+          <SwiperSlide key={i}>
+            <div className="flex flex-col items-center md:flex-row gap-x-8 h-full px-16">
+              {/* avatar, name, position  */}
+              <div className="w-full max-w-[300px] flex flex-col xl:justify-center items-center relative mx-auto xl:mx-0">
+                <div>
+                  {/* avatar */}
+                  <div>avatar</div>
+
+                  {/* name  */}
+                  <div>name</div>
+
+                  {/* position  */}
+                  <div>position</div>
+                </div>
+              </div>
+              {/* message  */}
+              <div className=" flex1 flex flex-col justify-center before:w-[1px] xl:before:bg-white/20 xl:before:absolute xl:before:left-0 xl:before:h-[200px] relative xl:pl-20">
+                {/* quote icon  */}
+                <div className="mb-4">
+                  <FaQuoteLeft className="text-4xl xl:text-6xl text-white/20 mx-auto md:mx-0" />
+                </div>
+                {/* message  */}
+                <div className="xl:text-lg text-center md:text-left">
+                  {person.message}
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+        );
+      })}
+    </Swiper>
+  );
 };
 
 export default TestimonialSlider;
--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -1,5 +1,22 @@
+//components
+import TestimonialSlider from "../../components/TestimonialSlider";
+
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <div className="h-full bg-primary/30 py-32 text-center">
+      <div className="container mx-auto h-full flex flex-col justify-center">
+        {/* tittle  */}
+        <h2 className="h2 mb-8 xl:mb-0">
+          What clients <span className="text-accent">say.</span>
+        </h2>
+
+        {/* slider  */}
+        <div>
+          <TestimonialSlider />
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default Testimonials;
