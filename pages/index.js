7d7cc3901ee20cda6976f847832286f600a1ec2a
Dins2344 <dinson.cd@gmail.com>
2026-01-24 13:55:31 +0530

    Updated avatar image size and positons

--- pages/index.js
+++ pages/index.js
@@ -72,7 +72,7 @@ const Home = () => {
           animate="show"
           exit="hidden"
           transition={{duration:1,ease:'easeInOut'}}
-          className="w-full h-full max-w-[700px] max-h-[650px] absolute bottom-32 lg:bottom-0 lg:right-[8%]"
+          className="w-full h-full max-w-[700px] max-h-[600px] absolute bottom-32 lg:bottom-0 lg:right-[7%]"
         >
           <Avatar />
         </motion.div>
